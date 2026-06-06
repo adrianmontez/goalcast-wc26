cd45c7fb013654b8a47afb7a97efbd7282eece98
Adrian Montez <adriancmontez@gmail.com>
2026-06-06 15:03:58 -0500

    revising back button

--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { groups } from "@/data/wc2026Data";
 import { teamMeritsByName } from "@/data/teamMerits";
 
